0f2a824fb316917748cc54104071ebef4ed5f8b8
kristina <kristina.mix93@gmail.com>
2024-04-14 22:59:20 +0300

    homework_13_

--- homework_13/task_from_lesson_13.py
+++ homework_13/task_from_lesson_13.py
@@ -1,15 +1,10 @@
 # 3) Напишите программу, которая принимает на вход список
 # чисел в одной строке и выводит на экран
 # в одну строку значения, которые встречаются в нём более одного раза.
-import random
 
 
 def digit_favorite(numbers):
-    list_digit_favorite = numbers.split(' ')
-    list_new = []
-    for number in list_digit_favorite:
-        if list_digit_favorite.count(number) > 1:
-            list_new.append(number)
+    list_new = [i for i in numbers.split(' ') if numbers.split(' ').count(i) > 1]
     set_list = ' '.join(list(set(list_new)))
     print(f'Значения, которые встречаются в строке более одного раза: {set_list}')
 
